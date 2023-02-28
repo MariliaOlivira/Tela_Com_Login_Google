551ee3a6ea4bd3836ec7d283cb884eeffba460cd
Marilia <marilia.araujo@academico.ifpb.edu.br>
2023-02-28 20:01:03 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import jwt_decode from "jwt-decode";
 
 function App() {
+  function handlecallback(response){
+    console.log("Encodede JWT ID token:" + response.credential);
+    var userObj = jwt_decode(response.credential);
+    console.log("Decoded JWT ID token:" + userObj);
+  }
+  useEffect(() => {
+    // global google
+    google.accounts.id.initialize({
+      client_id: "721610298440-ni8ecb4m9ba910642270uvi59ldqbs9i.apps.googleusercontent.com",
+      callback: handlecallback
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      {theme: "outline", size: "large"}
+    )
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div id="signInDiv"></div>
     </div>
+
   );
 }
 
